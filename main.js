1a338caeea525b018a66ae7b431a99f1092d6eac
DilawarBaig <dilawaralibaig@gmail.com>
2023-09-25 14:10:24 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+var guests = ["Asad", "Juned", "Daud", "Bilal", "Zeeshan"];
+console.log("The number of guests invited to dinner are: " + guests.length);
